3004db314759c5d7312e04c9c01d20727afa5320
nous <zubkov_p@mail.ru>
2017-06-06 14:03:55 +0000

    complete edit profile

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+'use strict';
+$(function() {
+  
+  // profile
+  const clickBtnSave = function clickBtnSave(event) {
+    const props = ['username', 'password', 'email', 'firstName', 'lastName', 'initials'];
+    const user = {};
+    for (let prop of props) {
+      let value = $(`#${prop} input`).val();
+      if (prop === 'password') {
+        const oldPassword = $('#password [name = "oldPassword"]').val();
+        const newPassword = $('#password [name = "newPassword"]').val();
+        if (oldPassword) {
+          user.oldPassword = oldPassword;
+          user.newPassword = newPassword;
+        }
+      } else {
+        if (value !== event.data[prop]) {
+          user[prop] = value;
+        }
+      }
+    }
+    if (jQuery.isEmptyObject(user)) {
+      // ничего
+    } else {
+      $.ajax({
+			  url: "/profile",
+  			method: "POST",
+  		// 	data: JSON.stringify(user),
+  		  data: user,
+  			error: function(err) {
+          $('.msg p').text('Ошибка');
+  				$('.msg').removeClass('text-info').addClass('text-danger');
+  			},
+  			statusCode: {
+  				200: function() {
+  				  $('.msg p').text('Сохранено. Обновите страницу');
+  				  $('.msg').removeClass('text-info').addClass('text-success');
+  				  window.location.reload();
+  			},
+			}
+		});
+    }
+  };
+  
+  const clickBtnChange = function clickBtnChange() {
+    const props = ['username', 'password', 'email', 'firstName', 'lastName', 'initials'];
+    const oldUser = {};
+    for (let prop of props) {
+      let elem = $(`#${prop} span`);
+      if (prop === 'password') {
+        elem.after(`<input type="text" class="form-control input-sm" name="newPassword" placeholder="Новый пароль">`);
+        elem.after(`<input type="text" class="form-control input-sm op" name="oldPassword" placeholder="Старый пароль">`);
+      } else {
+        elem.after(`<input type="text" class="form-control input-sm" name="${prop}" value="${elem.text()}">`);
+        oldUser[prop] = elem.text();
+      }
+      elem.remove();
+    }
+    const btn = $('.profile #change');
+    btn.text('Сохранить');
+    $('.msg').removeClass('hidden');
+    
+    $('.profile').off('click', '#change', clickBtnChange);
+    $('.profile').on('click', '#change', oldUser, clickBtnSave);
+  };
+  
+  $('.profile').on('click', '#change', clickBtnChange);
+
+});
--- routes/profile.js
+++ routes/profile.js
@@ -2,12 +2,34 @@
 
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const ObjectId = require('mongodb').ObjectId;
 
 router.get('/', function(req, res, next) {
 	if (!req.session.userId) {
 	  res.redirect('/signin');  
 	}
 	res.render('profile');
+});
+
+router.post('/', function(req, res, next) {
+	if (!req.session.userId) {
+		res.redirect('/signin'); // перенести это и остальные подобные редиректы, в отдельную функцию middleware
+	}
+	const user = {};
+  const props = ['username', 'oldPassword', 'newPassword', 'email', 'firstName', 'lastName', 'initials'];
+  for (let prop of props) {
+    if (req.body[prop]) {
+      user[prop] = req.body[prop];
+    }
+  }
+  user.id = new ObjectId(req.session.userId);
+  User.update(user, function(err) {
+  	if (err) {
+  		next(err);
+  	}
+		res.status(200).redirect('/profile');
+  });
 });
 
 module.exports = router;
