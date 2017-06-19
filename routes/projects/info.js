992c02fa08b377b97d9166434acb2f6e557cdd60
nous <zubkov_p@mail.ru>
2017-06-19 09:36:38 +0000

    complete description task

--- routes/projects/info.js
+++ routes/projects/info.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const log = require('../../libs/log.js')(module);
 const getProject = require('../../models/project/getProject.js').getProject;
+const ObjectId = require('mongodb').ObjectId;
+const db = require('../../libs/db.js').get();
 
 router.get('/:id', function(req, res, next) {
   getProject(req.params.id, function(err, project) {
@@ -12,6 +14,22 @@ router.get('/:id', function(req, res, next) {
     }
     req.project = res.locals.project = project;
     res.render('info', { title: 'Описание'});
+  });
+});
+
+router.post('/:id', function(req, res, next) {
+  const projectId = new ObjectId(req.params.id);
+  const description = req.body.description;
+  db.projects.findOneAndUpdate({
+    _id: projectId
+  }, {
+    $set: { description: description }
+  },
+  function(err, r) {
+    if (err) {
+      return next(err);
+    }
+    res.end();
   });
 });
 
